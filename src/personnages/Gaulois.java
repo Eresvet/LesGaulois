d1ff86600889697b1e3094529588883165555513
Eresvet <76060145+Eresvet@users.noreply.github.com>
2023-09-20 14:28:16 +0200

    premier

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion;
+	
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+}
