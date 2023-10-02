d7977862ebe02ba619e341f0db860404721265d0
Anh tuan remi Sintes vu <snn4566a@ens.fsi>
2023-10-02 17:18:08 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,6 +4,8 @@ public class Romain {
 	
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		assert (force > 0);
@@ -36,12 +38,46 @@ public class Romain {
 		assert (forceDebut > forceFin);
 	}
 	
+	public void sEquiper(Equipement equip) {
+		switch(nbEquipement) {
+			case 1:
+				if (equipements[nbEquipement-1] == equip) {
+					System.out.println("Le soldat " + getNom() + " possede deja un " + equip + ".");
+
+					
+					
+				}else{
+					equipements[nbEquipement] = equip;
+					nbEquipement++;
+					System.out.println("Le soldat " + getNom() + " s'equipe avec un " + equip + ".");
+
+				}
+				break;
+			case 0:
+				equipements[nbEquipement] = equip;
+				nbEquipement++;
+				System.out.println("Le soldat " + getNom() + " s'equipe avec un " + equip + ".");
+
+				break;
+				
+			default:
+				System.out.println("Le soldat " + getNom() + " est deja bien protege !");
+
+
+		}
+	}
+	
 	public static void main(String[] args) {
 		Romain minus;
 		minus = new Romain("Minus", 6);
+//		
+//		minus.parler("Oui ");
+//		
+//		minus.recevoirCoup(8);
 		
-		minus.parler("Oui ");
-		
-		minus.recevoirCoup(8);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
